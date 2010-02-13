776748144a17ae937e284c9fc8470374d4fb23cd
kazunori279 <kazunori279@8718cb86-0a2d-11df-a26f-8b716316a5bc>
2010-02-13 11:35:55 +0000

    Share project "jsonengine" into "https://jsonengine.googlecode.com/svn"

---
+++ trunk/jsonengine/src/com/jsonengine/service/QueryService.java
@@ -0,0 +1,20 @@
+package com.jsonengine.service;
+
+/**
+ * Provides query service for jsonengine.
+ * 
+ * @author @kazunori_279
+ */
+public class QueryService {
+
+    /**
+     * Singleton instance.
+     */
+    public static final QueryService i = new QueryService();
+
+    private QueryService() {
+    }
+
+    
+    
+}
