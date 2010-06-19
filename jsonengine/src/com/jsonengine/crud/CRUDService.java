3c6245e0fa53cfc0541fc1b7265f080ecd5d5564
kazunori279 <kazunori279@8718cb86-0a2d-11df-a26f-8b716316a5bc>
2010-06-19 04:24:38 +0000

    Updated Slim3 to the latest (1.0.4)

--- jsonengine/src/com/jsonengine/crud/CRUDService.java
+++ jsonengine/src/com/jsonengine/crud/CRUDService.java
@@ -25,7 +25,7 @@ import com.jsonengine.model.JEDoc;
  */
 public class CRUDService {
 
-    /**
+    /*
      * Singleton instance.
      */
     public static final CRUDService i = new CRUDService();
